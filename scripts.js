f689ec9866719710e0370aa70c791d6d1ea1f2a2
ruscias <stevenwrusciano@gmail.com>
2022-11-08 15:46:28 -0600

    Add logic to check if button pressed will be logged, reorder functions
    I moved the if statements checking if the clear or backspace buttons were
    pressed, from main, into a function- isButtonToLog.
    
    I also moved functions around.
    
    I removed updateOperationsSinceClear from the clear function as it
    is no longer needed.
    
    I also renamed showButtonPressed to processButton to better match the
    intention of the function with the new order and logic.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ buttons.forEach( (button) => {
   button.addEventListener('click', main)
 });
 
-function clear(buttonPressed) {
+function clear() {
   const divButtonPressed = document.querySelector('div#button-pressed');
   divButtonPressed.innerText = '';  
   const divOperationsSinceClear = document.querySelector('div#operations-since-clear');
@@ -14,37 +14,40 @@ function clear(buttonPressed) {
   divOperationsSinceClear.innerText = buttonsPressedArray
 }
 
-function updateOperationsSinceClear() {
-  const divOperationsSinceClear = document.querySelector('div#operations-since-clear');
-  divOperationsSinceClear.innerText = buttonsPressedArray.join(' ');
-}
-
 function backspace() {
   if (buttonsPressedArray.length === 0) {
     return 0;
   }
   buttonsPressedArray.pop();
-  updateOperationsSinceClear();
-  console.log(buttonsPressedArray);
 }
 
-function showButtonPressed(buttonPressed) {
-  const divButtonPressed = document.querySelector('div#button-pressed');
-  divButtonPressed.innerText = buttonPressed;
-  buttonsPressedArray.push(buttonPressed);  
-}
-
-function main() {
-  if (this.id === 'clear') {
+function isButtonToLog(buttonId) {
+  if (buttonId === 'clear') {
     clear();
     return 0;
   }
 
-  if (this.id === 'backspace') {
+  if (buttonId === 'backspace') {
     backspace();
     return 0;
   }
-  // put those if statements in something - a function to check the buttons we don't want to log maybe?
-  showButtonPressed(this.innerText);
+}
+
+function processButton(buttonPressed) {
+  if (isButtonToLog(buttonPressed.id) === 0) {
+    return 0;
+  }
+  const divButtonPressed = document.querySelector('div#button-pressed');
+  divButtonPressed.innerText = buttonPressed.innerText;
+  buttonsPressedArray.push(buttonPressed.innerText);  
+}
+
+function updateOperationsSinceClear() {
+  const divOperationsSinceClear = document.querySelector('div#operations-since-clear');
+  divOperationsSinceClear.innerText = buttonsPressedArray.join(' ');
+}
+
+function main() {
+  processButton(this);
   updateOperationsSinceClear();
 } 
