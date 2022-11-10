7ff63358af7c2e442bca40d9edbad4659f166c82
ruscias <stevenwrusciano@gmail.com>
2022-11-09 20:20:30 -0600

    I just had to get through it. Sorry. No idea what I did.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ import { add, subtract, multiply, divide, exp  } from '/helpers.js';
 
 let buttonsSinceClear = [];
 let buttonsToCommit = '';
-let committedButtons = [];
 let numbersToProcess = [];
 let lastOperationRequested = '';
 
@@ -12,24 +11,24 @@ let lastOperationRequested = '';
 function clear() {
   buttonsSinceClear = [];
   buttonsToCommit = '';
-  committedButtons = [];
   numbersToProcess = [];
   lastOperationRequested = '';
 }
 
 function backspace () {
-  return 0;
+  if (buttonsToCommit.length > 0) {
+    buttonsToCommit = buttonsToCommit.slice(0,-1);
+  }
 }
 
 function commitNumbers() {
   buttonsSinceClear.push(buttonsToCommit);
-  buttonsToCommit = '';
   numbersToProcess.push(parseFloat(buttonsToCommit));
+  buttonsToCommit = '';
 }
 
 function processNumber(button) {
   buttonsToCommit += button.id;
-  lastOperationRequested = '';
 }
 
 function processDecimal() {
@@ -43,27 +42,73 @@ function processDecimal() {
 }
 
 function processPositiveOrNegative() {
-  return 0;
+  if (buttonsToCommit.length === 0) {
+    return 0
+  };
+  const adjustButtonsToCommit = parseFloat(buttonsToCommit) * -1;
+  buttonsToCommit = adjustButtonsToCommit.toString();
 }
 
 function processOperation(button) {
+
   if (buttonsToCommit.length > 0) {
     commitNumbers();
-  } else if (button.id === 'equals') {
-    return 0;  
-  } else {
-    lastOperationRequested = button.innerText;
-    buttonsSinceClear.push(lastOperationRequested)
   }
+  
+  if (button.id === 'equals') {
+    return 0;  
+  } 
+  
+  let lastOperationRequestedSymbol = button.innerText;
+  if (lastOperationRequested !== '') {
+    buttonsSinceClear.pop();
+  }
+  buttonsSinceClear.push(lastOperationRequestedSymbol)
+  lastOperationRequested = button.id;
+
 }
 
-function makeAnyCalculations() {
-  return 0;
+function makeAnyCalculations(operationRequested) {
+  if (numbersToProcess.length < 2) {
+    return 0;
+  }
+
+  let result = 0;
+  console.log(`operation ${operationRequested}`);
+  switch (operationRequested) {
+    case 'subtract':
+      result = subtract(numbersToProcess[0], numbersToProcess[1]);
+      break;
+    case 'add': 
+      result = add(numbersToProcess[0], numbersToProcess[1]);
+      break;
+    case 'multiply':
+      result = multiply(numbersToProcess[0], numbersToProcess[1]);
+      break;
+    case 'exp':
+      result = exp(numbersToProcess[0], numbersToProcess[1]);
+      break;
+    case 'divide':
+      result = divide(numbersToProcess[0], numbersToProcess[1]);
+      break;
+    default:
+      result = 0;
+  }
+  clear();
+  console.log(`result ${result}`);
+  buttonsToCommit = '';
+  buttonsSinceClear = [result];
+  numbersToProcess = [result];
+  lastOperationRequested = '';
 }
 
 function updateButtonsToCommit() {
   const divButtonsToCommit = document.querySelector('div.buttons-to-commit');
-  divButtonsToCommit.innerText = buttonsToCommit;
+  if (buttonsToCommit.length === 0) {
+    divButtonsToCommit.innerText = '0';
+  } else {
+    divButtonsToCommit.innerText = buttonsToCommit;
+  }
 }
 
 function updateButtonsSinceClear() {
@@ -78,7 +123,10 @@ function processButton() {
   if (this.id === 'clear') {
     clear();
   }
-  //if id = backspace
+  //handle backspace
+  if (this.id === 'backspace') {
+    backspace();
+  }
   //handle numbers
   if (this.className === 'number') {
     processNumber(this);
@@ -87,13 +135,16 @@ function processButton() {
   if (this.id === 'decimal') {
     processDecimal();
   }
-  //if id = positive-or-negative
+  //handle positive-or-negative button
+  if (this.id === 'positive-or-negative') {
+    processPositiveOrNegative();
+  }
   //handle operations
   if (this.className === 'operation') {
     processOperation(this);
   }
 
-  makeAnyCalculations();
+  makeAnyCalculations(lastOperationRequested);
 
   //update ui - buttons-to-commit to reflect any needed changes
   updateButtonsToCommit();
