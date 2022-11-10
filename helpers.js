bef5032fd7d6c988571829fbf923e5368e935a81
ruscias <stevenwrusciano@gmail.com>
2022-11-09 18:38:25 -0600

    Rewrite basic blueprint for code, add a helpers.js file

---
+++ helpers.js
@@ -0,0 +1,22 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function exp(a,b) {
+  return a ** b;
+}
+
+
+export {add, subtract, multiply, divide, exp };
--- scripts.js
+++ scripts.js
@@ -1,10 +1,67 @@
-function main() {
+import { add, subtract, multiply, divide, exp  } from '/helpers.js';
+
+
+let buttonsSinceClear = [];
+let buttonsToCommit = [];
+let committedButtons = [];
+let lastOperationRequested = '';
+
+
+
+function clear() {
+  return 0;
+}
+
+function backspace () {
+  return 0;
+}
+
+function commitButtons() {
+    return 0;
+}
+
+function processNumber() {
+  return 0;
+}
+
+function processPeriod() {
+  return 0;
+}
+
+function processPositiveOrNegative() {
+  return 0;
+}
+
+function processOperation() {
+  return 0;
+}
+
+function updateButtonsToCommit() {
+  return 0;
+}
+
+function updateButtonsSinceClear() {
+  return 0;
+}
+
+function processButton() {
   console.log(this);
+  //Call a function depending on the button pressed
+  //if id = clear
+  //if id = backspace
+  //if className = number 
+  //if id = period
+  //if id = positive-or-negative
+  //if className = operation
+
+  //update ui - buttons-to-commit to reflect any needed changes
+  //update ui - buttons-since-clear to reflect any needed changes 
+
 }
 
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach( (button) => {
-  button.addEventListener('click', main)
+  button.addEventListener('click', processButton)
 });
